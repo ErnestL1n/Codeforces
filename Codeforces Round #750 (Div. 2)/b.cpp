e20e98029de0b968180809e54b50566314a874bf
ErnestL1n <ernestlin1998@gmail.com>
2021-10-27 23:52:30 +0800

    Update b.cpp

--- Codeforces Round #750 (Div. 2)/b.cpp
+++ Codeforces Round #750 (Div. 2)/b.cpp
@@ -1,26 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+
+// TLE
+// ll bar(vector<ll>& arr, ll n, ll target) {
+// if (target < 0) {
+// return 0;
+// }
+// vector<vector<ll>> dp(n + 1, vector<ll>(target + 1));
+// for (ll i = 0; i <= n; ++i) {
+// for (ll j = 0; j <= target; ++j) {
+// if (i == 0) {
+// if (j == 0) {
+// dp[i][j] = 1;
+// }
+// } else {
+// ll begin = n - i;
+// if (arr[begin] > j) {
+// continue;
+// }
+// if (j >= arr[begin]) {
+// dp[i][j] = dp[i - 1][j - arr[begin]] + dp[i - 1][j];
+// } else {
+// dp[i][j] = dp[i - 1][j];
+// }
+// }
+// }
+// }
+// return dp[n][target];
+// }
+
+void foo() {
+  ll n, res = 0;
+  cin >> n;
+  vector<ll> arr(n);
+  for (ll i = 0; i < n; ++i) {
+    cin >> arr[i];
+    if (arr[i] == 1) {
+      ++res;
+    }
+  }
+  for (ll i = 0; i < n; ++i) {
+    if (arr[i] == 0) {
+      res *= 2;
+    }
+  }
+
+  cout << res << "\n";
+}
+
 int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
   int t;
   cin >> t;
-  for (int i = 0; i < t; i++) {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int j = 0; j < n; j++) {
-      cin >> a[j];
-    }
-    long long ans = 0;
-    for (int j = 0; j < n; j++) {
-      if (a[j] == 1) {
-        ans++;
-      }
-    }
-    for (int j = 0; j < n; j++) {
-      if (a[j] == 0) {
-        ans *= 2;
-      }
-    }
-    cout << ans << endl;
+  while (t--) {
+    foo();
   }
+  return 0;
 }
